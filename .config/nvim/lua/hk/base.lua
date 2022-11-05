ef559ee9124b2fcfcb227f384584d158ab664d08
Rahul Sharma <rsharma842756@gmail.com>
2022-11-05 22:32:35 +0530

    open nerdtree automatically in new tabs

--- .config/nvim/lua/hk/base.lua
+++ .config/nvim/lua/hk/base.lua
@@ -103,6 +103,9 @@ vim.cmd([[autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists
 -- If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
 vim.cmd([[autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 | let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif]])
 
+-- Open the existing NERDTree on each new tab.
+vim.cmd([[autocmd BufWinEnter * if getcmdwintype() == '' | silent NERDTreeMirror | endif]])
+
 g.NERDTreeShowHidden = 1
 
 -- Vim wiki
