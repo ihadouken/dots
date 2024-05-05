fd66dfe42410b52a90b1098a7021ad9615609308
Rahul Sharma <rsharma842756@gmail.com>
2024-05-05 09:48:23 +0530

    Re-enable blinking cursor in emacs

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -448,7 +448,7 @@ List of keybindings (SPC h b b)")
     (set-face-attribute 'org-table nil :font doom-font :weight 'normal :height 1.0 :foreground "#bfafdf"))
 
 ;; Load our desired dt/org-colors-* theme on startup
-(add-hook 'org-mode-hook 'dt/org-colors-gruvbox-dark)
+(add-hook 'org-mode-hook 'dt/org-colors-doom-one)
 
 (setq org-journal-dir "~/Documents/org/journal/"
       org-journal-date-prefix "* "
@@ -544,8 +544,10 @@ List of keybindings (SPC h b b)")
 (use-package! evil-terminal-cursor-changer
   :hook (tty-setup . evil-terminal-cursor-changer-activate))
 
+(blink-cursor-mode t)
+
 ;; Disable blinking cursor in tty.
-(setq visible-cursor nil)
+;; (setq visible-cursor nil)
 
 ;; Convenient mapping to toggle artist-mode
 ;; (map! :leader
@@ -605,7 +607,7 @@ List of keybindings (SPC h b b)")
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
