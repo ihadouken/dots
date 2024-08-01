c74621ec2de4290303fea9a85224f1e2e31eecb3
Rahul Sharma <rsharma842756@gmail.com>
2024-08-01 22:47:54 +0530

    feat(nvim): Smarter autoindent via treesitter

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -1,20 +1,18 @@
--------------------------------------------------
--- DT'S NEOVIM CONFIGURATION
--- Neovim website: https://neovim.io/
--- DT's dotfiles: https://gitlab.com/dwt1/dotfiles
--------------------------------------------------
-
 local status, ts = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+    return
 end
 
 ts.setup({
-	-- If TS highlights are not enabled at all, or disabled via `disable` prop,
-	-- highlighting will fallback to default Vim syntax highlighting
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = { "org" }, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
-	},
-	ensure_installed = { "vimdoc", "python", "bash", "comment", "c", "css", "haskell", "html", "json", "org", "lua", "java"}, -- Or run :TSUpdate org
+    -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+    -- highlighting will fallback to default Vim syntax highlighting
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "org" }, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+    },
+
+    ensure_installed = { "vimdoc", "python", "bash", "comment", "c", "css", "haskell", "html", "json", "org", "lua", "java"}, -- Or run :TSUpdate org
+    sync_install = false,
+
+    indent = { enable = true },
 })
